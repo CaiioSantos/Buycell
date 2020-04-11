42dfd5225338cd8b78b7b8aa137faab87e3fdeed
vitor <vitorn2009@hotmail.com>
2020-04-11 16:01:55 -0300

    Configurações firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkJOLySetfjT54EFQGyGSPWkaB5Pnaujw",
+    authDomain: "buycell-b75e7.firebaseapp.com",
+    databaseURL: "https://buycell-b75e7.firebaseio.com",
+    projectId: "buycell-b75e7",
+    storageBucket: "buycell-b75e7.appspot.com",
+    messagingSenderId: "836685532031",
+    appId: "1:836685532031:web:b102d5c9693046d210684a"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
