4763933e7214ffdd98e28fc1bd3c473fc28c45f1
CaiioSantos <caiohgs13@gmail.com>
2020-04-21 23:07:17 -0300

    CRUD PRODUTO

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import Todo from './components/Todo';
-
-function App() {
-  return (
-    <Todo />
-  );
-}
-=======
-  import React from "react";
+import React from "react";
 
 import Routes from "./routes";
->>>>>>> Adição do Crud de produtos.
 
 const App = () => <Routes />;
 
